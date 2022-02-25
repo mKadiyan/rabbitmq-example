5eca2ba8b5a6b5f6a9792b66a860bbeb933c6cc3
Ravinder Kadiyan <ravinder.kadiyan@gmail.com>
2022-02-25 22:36:05 +0530

    refactor a bit

--- rabbitmq-receiver/src/main/java/com/poc/receiver/listener/RabbitMQ.java
+++ rabbitmq-receiver/src/main/java/com/poc/receiver/listener/RabbitMQ.java
@@ -1,6 +1,6 @@
 package com.poc.receiver.listener;
 
-import com.poc.receiver.model.Notification;
+import com.poc.receiver.model.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +11,8 @@ public class RabbitMQ {
     private AtomicLong count = new AtomicLong(0L);
 
     @RabbitListener(queues = "${sample.rabbitmq.queue}")
-    public void recievedMessage(Notification notification) {
-        System.out.println("( "+count.incrementAndGet()+" ) Received = : " + notification);
+    public void recievedMessage(Message message) {
+        System.out.println("( "+count.incrementAndGet()+" ) Received = : " + message);
 
     }
 }
